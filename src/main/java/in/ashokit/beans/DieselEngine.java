95020d7274247b37bf58b9f0618b8b0e61208188
snehasonawane <sonawanesneha14792@gmail.com>
2022-08-05 19:53:00 +0530

    first commit

---
+++ src/main/java/in/ashokit/beans/DieselEngine.java
@@ -0,0 +1,15 @@
+package in.ashokit.beans;
+
+public class DieselEngine implements IEngine
+{
+
+	public DieselEngine() {
+		System.out.println("DieselEngine :: constructor");
+	}
+
+	public int start() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
---
+++ src/main/java/in/ashokit/beans/MyApp.java
@@ -0,0 +1,11 @@
+package in.ashokit.beans;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AppplicationContext context=new ClassPathXmlAppplicationContext();
+
+	}
+
+}
